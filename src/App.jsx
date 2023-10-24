0d3c8a38fa0e908030980d69a97e63a26db53c0e
ZakaNaji <zakaria.naji@edu.uca.ma>
2023-10-24 20:28:24 +0100

    Use composition to solve prop drilling, also to have a clear structure of our app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,178 +1,106 @@
+/* eslint-disable react/prop-types */
+import { tempMovieData, tempWatchedData } from "./data";
 import { useState } from "react";
 import "./App.css";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  return (
+    const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
+
+    return (
     <>
-      <NavBar />
-      <Main />
+        <NavBar>
+            <Logo />
+            <Searchbar />
+            <Results movies={movies}/>
+        </NavBar>
+        <Main>
+            <ListBox>
+                <MoviesList movies={movies} />
+            </ListBox>
+            <ListBox>
+                <WatchedMoviesSummory watched={watched} />
+                <WatchedMoviesList watched={watched} />
+            </ListBox>
+        </Main>
     </>
   );
 }
 
-const Main = () => {
+const Main = ({children}) => {
   return (
     <main className="main">
-      <MoviesListSection />
-
-      <WatchedMoviesSection />
+        {children}
     </main>
   );
 };
 
-function WatchedMoviesSection() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedMoviesSummory watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
-    </div>
-  );
-}
-
 function WatchedMoviesSummory({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
-  return (
-    <>
-      <div className="summary">
-        <h2>Movies you watched</h2>
-        <div>
-          <p>
-            <span>#️⃣</span>
-            <span>{watched.length} movies</span>
-          </p>
-          <p>
-            <span>⭐️</span>
-            <span>{avgImdbRating}</span>
-          </p>
-          <p>
-            <span>🌟</span>
-            <span>{avgUserRating}</span>
-          </p>
-          <p>
-            <span>⏳</span>
-            <span>{avgRuntime} min</span>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+    const avgUserRating = average(watched.map((movie) => movie.userRating));
+    const avgRuntime = average(watched.map((movie) => movie.runtime));
+    return (
+        <>
+            <div className="summary">
+                <h2>Movies you watched</h2>
+                <div>
+                    <p>
+                        <span>#️⃣</span>
+                        <span>{watched.length} movies</span>
+                    </p>
+                    <p>
+                        <span>⭐️</span>
+                        <span>{avgImdbRating}</span>
+                    </p>
+                    <p>
+                        <span>🌟</span>
+                        <span>{avgUserRating}</span>
+                    </p>
+                    <p>
+                        <span>⏳</span>
+                        <span>{avgRuntime} min</span>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 }
-
 function WatchedMoviesList({ watched }) {
-  return (
-    <ul className="list">
-      {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="list">
+            {watched.map((movie) => (
+                <WatchedMovie movie={movie} key={movie.imdbID} />
+            ))}
+        </ul>
+    );
 }
-
 function WatchedMovie({ movie }) {
-  return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-      </div>
-    </li>
-  );
+    return (
+        <li>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>⭐️</span>
+                    <span>{movie.imdbRating}</span>
+                </p>
+                <p>
+                    <span>🌟</span>
+                    <span>{movie.userRating}</span>
+                </p>
+                <p>
+                    <span>⏳</span>
+                    <span>{movie.runtime} min</span>
+                </p>
+            </div>
+        </li>
+    );
 }
 
-function MoviesListSection() {
-  const [isOpen1, setIsOpen1] = useState(true);
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
-      </button>
-      {isOpen1 && <MoviesList />}
-    </div>
-  );
-}
-
-function MoviesList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MoviesList({movies}) {
 
   return (
     <ul className="list">
@@ -198,12 +126,10 @@ function Movie({ movie }) {
   );
 }
 
-const NavBar = () => {
+const NavBar = ({children}) => {
   return (
     <nav className="nav-bar">
-      <Logo />
-      <Searchbar />
-      <Results />
+        {children}
     </nav>
   );
 };
@@ -221,7 +147,7 @@ function Searchbar() {
   );
 }
 
-function Logo({}) {
+function Logo() {
   return (
     <div className="logo">
       <span role="img">🍿</span>
@@ -230,10 +156,25 @@ function Logo({}) {
   );
 }
 
-function Results({}) {
+function Results({movies}) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+        {/* eslint-disable-next-line react/prop-types */}
+      Found <strong>{movies.length}</strong> results
     </p>
   );
+}
+
+function ListBox({children}) {
+    const [isOpen, setIsOpen] = useState(true);
+    return (
+        <div className="box">
+            <button
+                className="btn-toggle"
+                onClick={() => setIsOpen((open) => !open)}
+            >
+                {isOpen ? "–" : "+"}
+            </button>
+            {isOpen && children}
+        </div>);
 }
